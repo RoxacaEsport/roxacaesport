e8b85ae3d1c6ece39f2b9c5891de4c0b7487940a
RoxacaEsport <rxcroxaca@gmail.com>
2023-09-27 08:47:31 +0900

    Added New Files

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,323 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ROXACA E-Sports</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url('PodTalk') ?>/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="<?= base_url('PodTalk') ?>/css/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="<?= base_url('PodTalk') ?>/css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="<?= base_url('PodTalk') ?>/css/owl.theme.default.min.css">
+
+    <link href="<?= base_url('PodTalk') ?>/css/templatemo-pod-talk.css" rel="stylesheet">
+
+
+
+
+</head>
+
+<body style="background-color: black;">
+
+<style>
+    /* Add this CSS to your HTML */
+    .owl-carousel-info h4 {
+        font-size: 24px; /* Adjust the font size as needed */
+    }
+</style>
+
+
+    <main>
+
+        <nav class="navbar navbar-expand-lg bg-dark text-light">
+
+            <div class="container">
+                <a class="navbar-brand me-lg-5 me-0" href="">
+                    <img src="<?= base_url('PodTalk') ?>/images/ROXACA.png" class="logo-image img-fluid" alt="templatemo pod talk">
+                    <span class="brand-text font-weight-light"><b><b>ROXACA E-SPORTS</b></b></span>
+
+
+                </a>
+
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" style="color: gold; font-size: 20px;">Dominate the Battleground, Define the Victory</a>
+
+                        </li>
+                    </ul>
+                </div>
+
+
+
+            </div>
+        </nav>
+
+
+        <section class="hero-section">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="text-center mb-5 pb-2">
+                            <h1 class="text-white">PLAYERS</h1>
+                        </div>
+
+
+                        <div class="owl-carousel owl-theme">
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="<?= base_url('PodTalk') ?>/images/profile/vanio23.png" class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info">
+                                    <h4 class="mb-2">
+                                        Vanio23
+                                        <img src="<?= base_url('PodTalk') ?>/images/Ace.png" class="owl-carousel-verified-image img-fluid" alt="">
+                                    </h4>
+                                    <span class="badge">Sniper</span>
+                                </div>
+
+                                <div class="social-share">
+                                    <ul class="social-icon">
+                                        <li class="social-icon-item">
+                                            <a href="https://instagram.com/niltonisaac17?igshid=MzRlODBiNWFlZA==" class="social-icon-link bi-instagram" target="_blank"></a>
+                                        </li>
+                                        <li class="social-icon-item">
+                                            <a href="https://www.facebook.com/nilton.isaac.568?mibextid=ZbWKwL" class="social-icon-link bi-facebook" target="_blank"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+
+
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="<?= base_url('PodTalk') ?>/images/profile/im19.png" class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info">
+                                    <h4 class="mb-2">
+                                        Im19
+                                        <img src="<?= base_url('PodTalk') ?>/images/Conqueror.png" class="owl-carousel-verified-image img-fluid" alt="">
+                                    </h4>
+
+                                    <span class="badge">Support</span>
+
+                                    <span class="badge">Fragger</span>
+                                </div>
+
+                                <div class="social-share">
+                                    <ul class="social-icon">
+                                        <li class="social-icon-item">
+                                            <a href="https://instagram.com/rmrsxofficial?igshid=OGQ5ZDc2ODk2ZA==" class="social-icon-link bi-instagram" target="_blank"></a>
+                                        </li>
+
+                                        <li class="social-icon-item">
+                                            <a href="https://www.facebook.com/djrisario.ximenes?mibextid=ZbWKwL" class="social-icon-link bi-facebook" target="_blank"></a>
+                                        </li>
+
+                                        <li class="social-icon-item">
+                                            <a href="https://youtube.com/@rmrsx3625?si=iaz20FcQuqNfg2-M" class="social-icon-link bi-youtube" target="_blank"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="<?= base_url('PodTalk') ?>/images/profile/4173.png" class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info">
+                                    <h4 class="mb-2">
+                                        4173
+                                        <img src="<?= base_url('PodTalk') ?>/images/Ace.png" class="owl-carousel-verified-image img-fluid" alt="">
+                                    </h4>
+
+                                    <span class="badge">Rusher</span>
+
+                                    <span class="badge">Healer</span>
+                                </div>
+
+                                <div class="social-share">
+                                    <ul class="social-icon">
+                                        <li class="social-icon-item">
+                                            <a href="https://instagram.com/alsa_2519?igshid=MzRlODBiNWFlZA==" class="social-icon-link bi-instagram" target="_blank"></a>
+                                        </li>
+
+                                        <li class="social-icon-item">
+                                            <a href="https://www.facebook.com/amorze.costamartinez?mibextid=ZbWKwL" class="social-icon-link bi-facebook" target="_blank"></a>
+                                        </li>
+
+                                        
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="<?= base_url('PodTalk') ?>/images/profile/Namykaze.png" class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info">
+                                    <h4 class="mb-2">
+                                        Namykaze
+                                        <img src="<?= base_url('PodTalk') ?>/images/Conqueror.png" class="owl-carousel-verified-image img-fluid" alt="">
+                                    </h4>
+
+                                    <span class="badge">Rusher</span>
+                                </div>
+
+                                <div class="social-share">
+                                    <ul class="social-icon">
+                                        <li class="social-icon-item">
+                                            <a href="https://instagram.com/namykaze06?igshid=MzRlODBiNWFlZA==" class="social-icon-link bi-instagram" target="_blank"></a>
+                                        </li>
+
+                                        <li class="social-icon-item">
+                                            <a href="https://www.facebook.com/reinaldo.piper?mibextid=ZbWKwL" class="social-icon-link bi-facebook" target="_blank"></a>
+                                        </li>
+
+                                        
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="<?= base_url('PodTalk') ?>/images/profile/Vaxxy.png" class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info">
+                                    <h4 class="mb-2">
+                                        Vaxxy
+                                        <img src="<?= base_url('PodTalk') ?>/images/Conqueror.png" class="owl-carousel-verified-image img-fluid" alt="">
+                                    </h4>
+
+                                    <span class="badge">IGL</span>
+                                    <span class="badge">Support</span>
+                                </div>
+
+                                <div class="social-share">
+                                    <ul class="social-icon">
+                                        <li class="social-icon-item">
+                                            <a href="https://instagram.com/aldo_crr?igshid=MzRlODBiNWFlZA==" class="social-icon-link bi-instagram" target="_blank"></a>
+                                        </li>
+
+                                        <li class="social-icon-item">
+                                            <a href="https://www.facebook.com/aldo.correia?mibextid=ZbWKwL" class="social-icon-link bi-facebook" target="_blank"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="<?= base_url('PodTalk') ?>/images/profile/ramones.png" class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info">
+                                    <h4 class="mb-2">
+                                        Ramones
+                                        <img src="<?= base_url('PodTalk') ?>/images/Ace.png" class="owl-carousel-verified-image img-fluid" alt="">
+                                    </h4>
+
+                                    <span class="badge">Fragger</span>
+                                </div>
+
+                                <div class="social-share">
+                                    <ul class="social-icon">
+                                        <li class="social-icon-item">
+                                            <a href="https://instagram.com/jovanyo_ximenes8?igshid=MzRlODBiNWFlZA==" class="social-icon-link bi-instagram" target="_blank"></a>
+                                        </li>
+
+                                        <li class="social-icon-item">
+                                            <a href="https://www.facebook.com/amones.ximenes?mibextid=ZbWKwL" class="social-icon-link bi-facebook" target="_blank"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-12 mb-5 mb-lg-0">
+                    <div class="subscribe-form-wrap">
+                        <h6>Address</h6>
+                        <p>Beco Nu'u Tahan, Vila Verde, Vera Cruz - Dili - Timor Leste</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-12 mb-4 mb-md-0 mb-lg-0">
+                    <h6 class="site-footer-title mb-3">Contact</h6>
+
+                    <p class="mb-2">
+                        <strong class="d-inline me-2">Phone:</strong>
+                        <a href="tel:+67073241097">+670 73241097</a>
+                    </p>
+                    <p>
+                        <strong class="d-inline me-2">Email:</strong>
+                        <a href="mailto:rxcroxaca@gmail.com">rxcroxaca@gmail.com</a>
+                    </p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-12">
+
+                    <h6 class="site-footer-title mb-3">Social Media</h6>
+
+                    <ul class="social-icon">
+                        <li class="social-icon-item">
+                            <a href="https://instagram.com/rxc_esport?igshid=MzRlODBiNWFlZA==" class="social-icon-link bi-instagram" target="_blank"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="https://www.facebook.com/ROXACAesport?mibextid=ZbWKwL" class="social-icon-link bi-facebook" target="_blank"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="https://youtube.com/@roxacae-sport4060?si=Bv7mCNd5_guu7jsV" class="social-icon-link bi-youtube" target="_blank"></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container pt-5">
+            <div class="row align-items-center">
+
+                <div class="col-lg-12 col-12">
+                    <p class="copyright-text mb-0">Copyright © 2023 ROXACA E-Sport
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="<?= base_url('PodTalk') ?>/js/jquery.min.js"></script>
+    <script src="<?= base_url('PodTalk') ?>/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url('PodTalk') ?>/js/owl.carousel.min.js"></script>
+    <script src="<?= base_url('PodTalk') ?>/js/custom.js"></script>
+
+</body>
+
+</html>
